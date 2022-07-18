5f0afc36cb8137e73a49d85140617ff4bbee6e6c
Nikolay Antonovich <TesaloNick@gmail.com>
2022-07-18 16:55:04 +0300

    add extending of classes

--- js/script.js
+++ js/script.js
@@ -5,13 +5,62 @@ const add = document.querySelector('.form__button')
 const list = document.querySelector('.content__list')
 const contentHead = document.querySelector('.content__head')
 
-class Todo {
+class ContentHead {
   constructor() {
     this.tasksArray = JSON.parse(localStorage.getItem('tasks')) || []
     this.contentHeadArray = JSON.parse(localStorage.getItem('contentHead')) || [{
       value: 'all',
       active: true,
     }]
+  }
+
+  printContentHead() {
+    contentHead.innerHTML = ''
+    this.contentHeadArray.map(item => {
+      const div = document.createElement('div');
+      div.classList.add('content__head_item');
+      div.innerHTML = item.value
+      item.active ?
+        div.classList.add('active') :
+        div.classList.remove('active')
+      contentHead.append(div);
+    })
+  }
+
+  changeContentHead(e) {
+    if (e.target.closest('.content__head_item')) {
+      this.contentHeadArray.map(item => { //print animation of ghanging group
+        if (e.target.textContent === item.value) {
+          item.active = true
+        } else {
+          item.active = false
+        }
+      })
+
+      if (e.target.textContent === 'all') {
+        console.log(this.tasksArray);
+        this.printTasks(this.tasksArray)
+      } else {
+        let newTasksArray = this.tasksArray.filter(item => e.target.textContent === item.condition.value)
+        this.printTasks(newTasksArray)
+      }
+
+      localStorage.setItem('contentHead', JSON.stringify(this.contentHeadArray))
+      this.printContentHead()
+    }
+  }
+
+  getContentHeadArray() {
+    localStorage.setItem('contentHead', JSON.stringify(this.contentHeadArray))
+    if (JSON.parse(localStorage.getItem('contentHead')).length <= 1) {
+      for (let item of select.children) {
+        this.contentHeadArray.push({
+          value: item.textContent,
+          active: false
+        })
+      }
+    }
+    localStorage.setItem('contentHead', JSON.stringify(this.contentHeadArray))
   }
 
   printTasks(array) {
@@ -42,28 +91,42 @@ class Todo {
       }
     })
   }
+}
+
+class Todo extends ContentHead {
+  constructor() {
+    super()
+  }
+
 
   add(e) {
     e.preventDefault();
+    if (input.value.length > 0) {
+      let itemProgressColor = ''    // find progress color of item
+      for (let i = 0; i < select.length; i++) {
+        if (select[i].value === select.value) itemProgressColor = select[i].dataset.color;
+      }
 
-    let itemProgressColor = ''    // find progress color of item
-    for (let i = 0; i < select.length; i++) {
-      if (select[i].value === select.value) itemProgressColor = select[i].dataset.color;
+      this.tasksArray.push({
+        number: Math.round(Math.random() * 10000000),
+        checked: false,
+        value: input.value,
+        condition: {
+          value: select.value,
+          color: itemProgressColor,
+        },
+      })
+
+      localStorage.setItem('tasks', JSON.stringify(this.tasksArray))
+      this.printTasks(this.tasksArray)
+      console.log(this.tasksArray);
+      input.value = '';
+      input.placeholder = 'input task';
+      input.classList.remove('wrong')
+    } else {
+      input.placeholder = 'ERROR. Input value';
+      input.classList.add('wrong')
     }
-
-    this.tasksArray.push({
-      number: Math.round(Math.random() * 10000000),
-      checked: false,
-      value: input.value,
-      condition: {
-        value: select.value,
-        color: itemProgressColor,
-      },
-    })
-    localStorage.setItem('tasks', JSON.stringify(this.tasksArray))
-    this.printTasks(this.tasksArray)
-    console.log(this.tasksArray);
-    input.value = '';
   }
 
   check(e) {
@@ -85,7 +148,7 @@ class Todo {
     if (e.type === 'keypress' && e.key === "Enter" && e.target.closest('.item__text')) {
       e.preventDefault();
       this.tasksArray.map(item => {
-        if (+e.target.closest('.item').querySelector('.item__text').dataset.number === +item.number) {
+        if (e.target.closest('.item').querySelector('.item__text').dataset.number == item.number) {
           item.value = e.target.value
         }
       })
@@ -108,7 +171,7 @@ class Todo {
 
   delete(e) {
     if (e.target.closest('.item__delete')) {
-      let resultArray = this.tasksArray.filter(item => +e.target.closest('.item').querySelector('.item__text').dataset.number !== +item.number)
+      let resultArray = this.tasksArray.filter(item => e.target.closest('.item').querySelector('.item__text').dataset.number != item.number)
       this.tasksArray = resultArray
 
       localStorage.setItem('tasks', JSON.stringify(this.tasksArray))
@@ -116,58 +179,13 @@ class Todo {
       console.log(this.tasksArray);
     }
   }
-
-  printContentHead() {
-    contentHead.innerHTML = ''
-    this.contentHeadArray.map(item => {
-      const div = document.createElement('div');
-      div.classList.add('content__head_item');
-      div.innerHTML = item.active ?
-        `<div class="content__head_item active">${item.value}</div>` :
-        `<div class="content__head_item">${item.value}</div>`
-      contentHead.append(div);
-    })
-  }
-
-  changeContentHead(e) {
-    if (e.target.closest('.content__head_item')) {
-      this.contentHeadArray.map(item => { //print animation of ghanging group
-        if (e.target.textContent === item.value) {
-          item.active = true
-        } else {
-          item.active = false
-        }
-      })
-
-      if (e.target.textContent === 'all') {
-        this.printTasks(this.tasksArray)
-      } else {
-        let newTasksArray = this.tasksArray.filter(item => e.target.textContent === item.condition.value)
-        this.printTasks(newTasksArray)
-      }
-
-      localStorage.setItem('contentHead', JSON.stringify(this.contentHeadArray))
-      this.printContentHead()
-    }
-  }
-
-  getContentHeadArray() {
-    localStorage.setItem('contentHead', JSON.stringify(this.contentHeadArray))
-    if (JSON.parse(localStorage.getItem('contentHead')).length <= 1) {
-      for (let item of select.children) {
-        this.contentHeadArray.push({
-          value: item.textContent,
-          active: false
-        })
-      }
-    }
-    localStorage.setItem('contentHead', JSON.stringify(this.contentHeadArray))
-  }
 }
 
+let contentHeadClass = new ContentHead();
+contentHeadClass.getContentHeadArray()
+contentHeadClass.printContentHead()
+contentHead.addEventListener("click", (e) => contentHeadClass.changeContentHead(e))
 let task = new Todo();
-task.getContentHeadArray()
-task.printContentHead()
 task.printTasks(task.tasksArray)
 form.addEventListener('submit', (e) => task.add(e));
 list.addEventListener('click', (e) => task.delete(e));
@@ -176,4 +194,3 @@ list.addEventListener('click', (e) => task.addInputForChanging(e));
 list.addEventListener('click', (e) => task.check(e));
 list.addEventListener("keypress", (e) => task.changeItem(e))
 list.addEventListener("click", (e) => task.changeItem(e))
-contentHead.addEventListener("click", (e) => task.changeContentHead(e))
